c151066c0042f7d6954a3ab2fcdc361c5fea56db
hollandjb <hollandjb@vcu.edu>
2019-08-10 19:19:28 -0400

    restructured for mongoose/ejs build instead of sql/handlebars

--- app.js
+++ app.js
@@ -8,24 +8,36 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
 app.use(express.urlencoded({
   extended: true
 }));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get("/", (req, res) => {
   res.render("landing");
 })
 
-app.set("view engine", "ejs");
 
 
+app.get("/login", (req, res) => {
+  res.render("login");
+})
+
+app.get("/register", (req, res) => {
+  res.render("register");
+})
+
+app.get("/about", (req, res) => {
+  res.render("about");
+})
 
 
-
-
-
-
+app.post("/search", (req, res) => {
+  res.render("search");
+})
 
 
 
